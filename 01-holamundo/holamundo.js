eb6cbc669a51bf7612d554b49beb04da42fc6509
Dolly Aguirre <aguirredolly618@gmail.com>
2025-09-02 20:12:41 -0500

    creacion del javaScript

---
+++ 01-holamundo/holamundo.js
@@ -0,0 +1,27 @@
+console.log('hola mundo');
+
+let cajaDeAndy="woody";
+
+console.log(cajaDeAndy);
+
+
+let edad= 30;
+let nombre= "Dolly";
+
+console.log("mi nombre es",nombre,"y tengo",edad);
+
+//array
+
+let frutas = ['manzana', 'banano','mango'];
+
+console.log(frutas[1]);
+
+let frase = 'JavaScript es extremadamente genial';
+console.log(frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+//operaciones matematicas
+const suma= 3 + 2;
+console.log(suma);
